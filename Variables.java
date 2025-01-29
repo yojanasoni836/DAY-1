cd1f85d57001cda6fef0e30a6d27444e91eebe4b
yojanasoni836 <yojanasoni836@gmail.com>
2025-01-29 11:03:34 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,27 @@
+public class Variables{
+public static void main (String[]args){
+    byte byteVar = 10;
+    short shortVar = 100;
+    int intVar = 1000;
+    long longVar = 100000l;
+    float floatVar = 20.4f;
+    double doubleVar = 34.6;
+    char charVar = 'a';
+    boolean boolVar = true;
+    String stringVar = "Hello,yojana!";
+    System.out.println("byte value:"+ byteVar);
+    System.out.println("short value:"+ shortVar);
+    System.out.println("int value:"+ intVar);
+    System.out.println("long value:"+ longVar);
+    System.out.println("float value:"+ floatVar);
+    System.out.println("double value:"+ doubleVar);
+    System.out.println("char value:"+ charVar);
+    System.out.println("bool value:"+ boolVar);
+    System.out.println("string value:"+ stringVar);
+
+
+}
+}
+
+
+
